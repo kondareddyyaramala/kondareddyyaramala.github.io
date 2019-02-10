c9247df7f70eef23e860c1b561c3641c25045038
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2019-02-09 21:08:37 -0600

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { SkillSetComponent } from './components/skill-set/skill-set.component';
+import { PillComponent } from './components/pill/pill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    ProjectCardComponent,
+    SkillSetComponent,
+    PillComponent
   ],
   imports: [
     BrowserModule,
