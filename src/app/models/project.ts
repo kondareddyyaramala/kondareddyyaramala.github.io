54f4178223687f79e00984dea06fa8bf15a7901e
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2019-02-16 21:15:37 -0600

    Added in duties

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -2,11 +2,13 @@ export class Project {
     clientName: string;
     duration: string;
     description: string;
+    duties: Array<string>;
 
     constructor(config) {
         config = config || {};
         this.clientName = config.clientName;
         this.duration = config.duration;
         this.description = config.description;
+        this.duties = config.duties;
     }
 }
