86edba3212ad6e478b7c368b671dcb6515f1c825
kondareddy.yaramala@gmail.com <Konda.Yaramala@uline.com>
2019-03-01 17:05:59 -0600

    Added avatar images for the projects

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Project } from 'src/app/models/project';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'project-card',
@@ -8,4 +9,23 @@ import { Project } from 'src/app/models/project';
 })
 export class ProjectCardComponent {
   @Input() project: Project;
+
+  public backGroundImg: SafeStyle;
+
+  constructor(private sanitizer: DomSanitizer) {
+   }
+
+  getImageString(): string {
+    if (!this.project || !this.project.clientName) {
+      return null;
+    }
+    return this.project.clientName.toLowerCase().split(' ').join('').toString().concat('.png');
+  }
+
+
+  ngOnChanges(changes) {
+    if (changes && changes.project) {
+      this.backGroundImg = this.sanitizer.bypassSecurityTrustStyle(`url('../../../assets/${this.getImageString()}')`)
+    }
+  }
 }
